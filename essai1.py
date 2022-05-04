4d8551bd072cbf9691ac0e41cd67494af85ddb1d
Salma <salma.baiss@ens.uvsq.fr>
2022-05-04 16:57:43 +0200

    taille

--- essai1.py
+++ essai1.py
@@ -5,3 +5,22 @@
 #SALIC Awena
 #https://github.com/uvsq22101985/projetIN200N
 ######################
+
+import tkinter as tk
+from turtle import width
+
+#création de l'objet
+fenetre = tk.Tk()
+fenetre.title("Puissance 4")
+
+ #définition de la taille
+height = 500
+width = 500
+
+#taille minimum et maximum
+fenetre.minsize(700, 700)
+fenetre.maxsize(1000, 1000)
+
+fenetre.mainloop()
+
+
